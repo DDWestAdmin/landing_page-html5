4970813d72a1c5847e6f660228414418c147724e
[Diego <diego.donoso27@inacapmail.cl>
2026-05-05 13:48:43 -0400

    //

--- Jvscript/formulario.js
+++ Jvscript/formulario.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() { // Espera a que el DOM esté listo
-    const formulario = document.getElementById("formulario_usuario");
-    const registro = document.getElementById("boton_registro");
-    const botonregistro = document.getElementById("button_register");
-    const inicioSesion = document.getElementById("boton_inicio_sesion");
-    const botonInicioSesion = document.getElementById("button_signing");
+    const formulario = document.getElementById("formulario_usuario"); // Referencia al formulario de registro
+    const registro = document.getElementById("boton_registro"); // Referencia al botón de registro
+    const botonregistro = document.getElementById("button_register"); // Referencia al botón de envío del formulario de registro
+    const inicioSesion = document.getElementById("boton_inicio_sesion"); // Referencia al botón de inicio de sesión
+    const botonInicioSesion = document.getElementById("button_signing"); // Referencia al botón de envío del formulario de inicio de sesión
 });
 
