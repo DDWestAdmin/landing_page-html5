bbf22596290cb435569da9c7abba5875fa0aac55
[Diego <diego.donoso27@inacapmail.cl>
2026-05-05 13:37:44 -0400

    creacion modal

---
+++ Jvscript/formulario.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() { // Espera a que el DOM esté listo
+    const formulario = document.getElementById("formulario_usuario");
+    const registro = document.getElementById("boton_registro");
+    const botonregistro = document.getElementById("button_register");
+    const inicioSesion = document.getElementById("boton_inicio_sesion");
+    const botonInicioSesion = document.getElementById("button_signing");
+
+    formulario.
