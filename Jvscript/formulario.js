7ebf5c6d4b501a6d8ef07f7d14e0738dd4a25dcf
[Diego <diego.donoso27@inacapmail.cl>
2026-05-12 03:31:31 -0400

    adicion ia_consultas

--- Jvscript/formulario.js
+++ Jvscript/formulario.js
@@ -1,1 +1,9 @@
- 
+ document.addEventListener("DOMContentLoaded", function() {
+    const usuario = document.getElementById("registro_usuario");
+    const correo = document.getElementById("registro_email");
+    const contraseña = document.getElementById("registro_password");
+    const confirmar_contraseña = document.getElementById("registro_password_confirm");
+
+    let error = [];
+
+    if (usuario.lenght )
